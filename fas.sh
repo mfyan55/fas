2f6609f5f45b9223ddc59ba64198283940290b3c
Semi尼玛 <154477274@qq.com>
2018-10-20 07:31:57 +0800

    更新 fas.sh

--- fas.sh
+++ fas.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo '正在获取脚本...'
-wget https://gitee.com/yuqingkj/FAS4.0/raw/master/lyfas.sh;bash lyfas.sh
+echo '破解作者：Semi尼玛 QQ：154477274 群：104911190'
+echo '有问题请加群讨论'
+wget https://gitee.com/seminima/fas/blob/master/nmfas.sh;bash nmfas.sh
 
 echo '#盗版屏蔽dingd.cn
 127.0.0.1 www.dingd.cn
@@ -11,6 +13,7 @@ echo '#盗版屏蔽dingd.cn
 127.0.0.1 api.dingd.cn' >> /etc/fas_host
 
 rm -f /var/www/html/admin/access.php
+
 
 
 
